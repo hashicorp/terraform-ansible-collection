dc01fffe375e65b0f4a661e69fa87d85c90e0b5f
kaushiki069 <kausingh@redhat.com>
2025-07-01 16:17:27 +0530

    removing state from param

--- plugins/modules/terraform_configuration_version.py
+++ plugins/modules/terraform_configuration_version.py
@@ -23,33 +23,26 @@ options:
   state:
     description: The action to be performed for the configuration version.
     type: str
-    state: present/absent
   workspace:
     description: The workspace id for which the configuration version needs to be created.
     type: str
-    state: present
   auto-queue-runs: When true, runs are queued automatically when the configuration version is uploaded.
     type: boolean
     default: true
-    state: present
   speculative: When true, this configuration version may only be used to create runs which are speculative which cannot be confirmed or applied.
     type: boolean
     default: false
-    state: present
   provisional: When true, this configuration version does not immediately become the workspace current configuration version. If the associated run is applied, it then becomes the current configuration version unless a newer one exists.
     type: boolean
     default: false
-    state: present
   configuration_version_id:
     description: The id of the configuration version that needs to be archived.
     type: str
     required: false
-    state: absent
   archive:
     description: The option states if archive needs to be performed on the configuration version. Since deletion is not a supported
     option currently, hence this parameter is a required option as it is the only supported option for state 'absent' currently.
     type: bool
-    state: absent
 
 """
 
