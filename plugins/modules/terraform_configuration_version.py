40495b05440e8c7227ebab999e82ce8d9d66d0c3
kaushiki069 <kausingh@redhat.com>
2025-07-12 17:30:59 +0530

    modularize code

--- plugins/modules/terraform_configuration_version.py
+++ plugins/modules/terraform_configuration_version.py
@@ -114,6 +114,28 @@ from ansible_collections.hashicorp.terraform.plugins.module_utils.configuration 
 )
 
 
+def create_configuration_version(client_terraform, params, module):
+    try:
+        payload = {
+            "data": {
+                "type": "configuration-versions",
+                "attributes": {
+                    "auto-queue-runs": params["auto_queue_runs"],
+                    "speculative": params["speculative"],
+                    "provisional": params["provisional"],
+                },
+            }
+        }
+        config_version = create_config(client_terraform, params["workspace"], payload)
+        config_version_id = config_version.get("data").get("data", {}).get("id")
+        upload_url = (
+            config_version.get("data").get("data", {}).get("attributes", {}).get("upload-url")
+        )
+        return config_version_id, upload_url
+    except Exception as e:
+        module.fail_json(msg=str(e))
+
+
 def main():
     module = TerraformModule(
         argument_spec=dict(
@@ -129,38 +151,16 @@ def main():
     warnings = []
     result = {"changed": False, "warnings": warnings}
     params = module.params
-    if params["state"] == "present" and "workspace" not in params:
-        module.fail_json(msg="The 'workspace' parameter is required when state is 'present'.")
-    if params["state"] == "absent" and "archive" not in params:
-        module.fail_json(msg="The 'archive' parameter is required when state is 'absent'.")
     client = TerraformClient(tf_hostname=params["tf_hostname"], tf_token=params["tf_token"])
     try:
         if params["state"] == "present":
-            payload = {
-                "data": {
-                    "type": "configuration-versions",
-                    "attributes": {
-                        "auto-queue-runs": params["auto_queue_runs"],
-                        "speculative": params["speculative"],
-                        "provisional": params["provisional"],
-                    },
-                }
-            }
-            config_version = create_config(client, params["workspace"], payload)
-            config_version_id = config_version.get("data").get("data", {}).get("id")
-            config_version_status = (
-                config_version.get("data").get("data", {}).get("attributes", {}).get("status")
-            )
-            upload_url = (
-                config_version.get("data").get("data", {}).get("attributes", {}).get("upload-url")
-            )
+            config_version_id, upload_url = create_configuration_version(client, params, module)
             result.update(
                 {
                     "changed": True,
                     "msg": "Configuration version created successfully.",
                     "configuration_version_id": config_version_id,
                     "upload_url": upload_url,
-                    "configuration_version_status": config_version_status,
                 }
             )
             module.exit_json(**result)
