46f4e5a5979bbe14a47b0e2a3ec82cef9a43a144
Shashank Venkat <shvenkat@redhat.com>
2025-07-22 14:06:42 +0530

    Update exceptions.py

--- plugins/module_utils/exceptions.py
+++ plugins/module_utils/exceptions.py
@@ -1,3 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2024 Red Hat, Inc.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 from ansible.module_utils.urls import SSLValidationError
 
 
