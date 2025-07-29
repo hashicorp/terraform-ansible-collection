9a654ae25219b9de210075ec267ceac3c8b9c687
Kaushiki Singh <kausingh@redhat.com>
2025-07-29 19:09:10 +0530

    [AAP-50296] Adds `hashicorp.terraform.configuration_version` module (#13)
    * adding files
    
    * fixes
    
    * lint fixes
    
    * additions
    
    * adding satte archive
    
    * modularize code
    
    * addition of workspace name and org param
    
    * added docstring changes
    
    * adding combined logic
    
    * adding changes to docstring
    
    * update
    
    * updating file
    
    * adding change in param name
    
    * updating vars
    
    * changing module name
    
    * change to retries
    
    * update docstring
    
    * changing object
    
    * adding addtional check
    
    * updating for retries
    
    * updating if condition
    
    * removing .tf and .tf.json single file support logic
    
    * fixing docstring
    
    * fixing docstring
    
    * sanity fixes
    
    * shifting validate file call before create config
    
    * adding alias
    
    * sanity fix
    
    * removing unwanted params
    
    * adding required together
    
    * adding logic for expand path
    
    * updating module to address comments
    
    * removing extra function to fix sanity checks
    
    * temp
    
    * adding integration tests
    
    * fixing lint
    
    * fixed-modulenotfound-errors
    
    * adding sanity fix
    
    * deleted-common
    
    * added-common
    
    * sanity fix
    
    * added-mock-req
    
    * updated-common
    
    * fixed-ci.yml
    
    * quick fix
    
    * another-fix
    
    * fixes
    
    * fix
    
    * updated-unit-tests
    
    * adding updates to common
    
    * adding doc fragments
    
    * adding fragments and author
    
    * sanity fixes
    
    * fixing sanity checks
    
    * changes to address reviews
    
    * reverting integration check from workflow
    
    * reverting chnage
    
    * achive scenario handling
    
    * integration test fix
    
    * lint-fixes
    
    * reverting change added
    
    * modularizing main
    
    * toml fix
    
    * removing molecule
    
    * fix
    
    * adding licence, type checking
    
    * refactor
    
    * updating logic for get cv
    
    * fix
    
    * fstring fix
    
    * sanity fix
    
    * fixing sanity
    
    * adding integration test fixes
    
    * adding changes to address reviews
    
    * update
    
    * updating integration tests
    
    * cosmetic changes
    
    * update
    
    * address doc changes comments
    
    * updating polling logic
    
    * sanity fix
    
    * sanity fix
    
    * adding updates to address PR comments
    
    * changelog fix
    
    * handling edge case
    
    * fixed-module-unit-test
    
    * linting-fix
    
    * fixed-module-util-test
    
    * fix-coverage-issues
    
    * lint-fix
    
    * quick-fixes-test
    
    * updating integration tests
    
    * fixin sonaqube issues
    
    * fixing integration tests
    
    * clean-up-tests
    
    * removed-import-handling-tests
    
    * lint-fix
    
    * fix-corrupt-tar-gz
    
    * removed-import-tests
    
    * updating integration tests
    
    * clean-up
    
    * revert
    
    * removed-import-var
    
    * revert
    
    * adding return type
    
    * adding updates to docstring
    
    * address review comments
    
    Signed-off-by: NilashishC <nchakrab@redhat.com>
    
    * Add a no_log
    
    Signed-off-by: NilashishC <nchakrab@redhat.com>
    
    * fix lint
    
    Signed-off-by: NilashishC <nchakrab@redhat.com>
    
    * fix lint (again)
    
    Signed-off-by: NilashishC <nchakrab@redhat.com>
    
    * Raise TerraformError
    
    Signed-off-by: NilashishC <nchakrab@redhat.com>
    
    * rm errorneuos file
    
    Signed-off-by: NilashishC <nchakrab@redhat.com>
    
    * Remove "included content"
    
    Signed-off-by: NilashishC <nchakrab@redhat.com>
    
    * update module documentation
    
    Signed-off-by: NilashishC <nchakrab@redhat.com>
    
    * update module documentation
    
    Signed-off-by: NilashishC <nchakrab@redhat.com>
    
    * removing pre-commit hook and .rst
    
    * updating archivist client object and client
    
    * fix isort
    
    * updated-test-cases
    
    * updated_archivist_tests
    
    * fixed-unit-tests-again
    
    * added-few-tests
    
    * updating error message
    
    * updating docstring
    
    * Add tests/config.yaml
    
    Signed-off-by: NilashishC <nchakrab@redhat.com>
    
    ---------
    
    Signed-off-by: NilashishC <nchakrab@redhat.com>
    Co-authored-by: Shashank Venkat <shvenkat@redhat.com>
    Co-authored-by: NilashishC <nchakrab@redhat.com>

---
+++ plugins/module_utils/workspace.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2025 Red Hat, Inc.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from ansible_collections.hashicorp.terraform.plugins.module_utils.common import TerraformClient
+from ansible_collections.hashicorp.terraform.plugins.module_utils.exceptions import (
+    TerraformError,
+)
+
+
+def get_workspace(client: TerraformClient, organization: str, workspace_name: str):
+    """
+    Retrieves a specified workspace from Terraform Cloud.
+
+    Sends a GET request to fetch details of a workspace identified by its name
+    within a given organization. If the workspace is not found, returns an empty
+    dictionary. If successful, returns the workspace data with an added "status" field.
+    For any other error status, raises an HTTPError.
+
+    Args:
+        client (TerraformClient): An authenticated client used to interact with
+            the Terraform Cloud API.
+        organization (str): The name of the Terraform Cloud organization.
+        workspace_name (str): The name of the workspace to retrieve.
+
+    Returns:
+        dict: A dictionary containing the workspace data (with an added "status" field)
+        if found, or an empty dictionary if the workspace is not found (status 404).
+
+    Raises:
+        TerraformError: If the request fails with a non-404 status code.
+    """
+    response = client.get(f"/organizations/{organization}/workspaces/{workspace_name}")
+    response_data = response.get("data", {})
+    response_status = response["status"]
+
+    if response_status == 404:
+        # workspace was not found
+        # This should not raise an exception
+        return {}
+    elif response_status == 200:
+        # workspace was fetched successfully
+        response_data.update({"status": response_status})
+        return response_data
+    else:
+        # A failure status code was received when attempting to fetch the specified configuration version
+        # there can be several reasons for this so we raise an exception with the response
+        raise TerraformError(response)
