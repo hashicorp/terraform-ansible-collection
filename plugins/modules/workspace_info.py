d35df362640666efc431bc330f3c066e54a58eb0
Shashank Venkat <shvenkat@redhat.com>
2025-08-11 19:03:26 +0530

    first_push

---
+++ plugins/modules/workspace_info.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2025 Red Hat, Inc.
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, annotations, division, print_function
+
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+---
+module: workspace_info
+version_added: 1.0.0
+short_description: Gather information about a workspace in Terraform Enterprise/Cloud.
+author: "Shashank Venkat (@shvenkat)"
+description:
+  - This module retrieves information about a given workspace in Terraform Enterprise/Cloud.
+  - It can be used to check if a workspace exists or to gather detailed information about it.
+  - If I(workspace_id) is provided, it will return information about that specific workspace.
+  - If I(workspace_name) and I(organization_name) are provided, it will return information about the workspace with that name in the specified organization.
+  - Either I(workspace_id) or both I(workspace_name) and I(organization_name) must be provided.
+  - If the workspace does not exist, the module will fail with an error message.
+extends_documentation_fragment: hashicorp.terraform.common
+options:
+  workspace_id:
+    description:
+      - The unique identifier of the workspace to retrieve information about.
+      - This is mutually exclusive with I(workspace_name) and I(organization_name).
+    type: str
+    required: false
+  workspace_name:
+    description:
+      - The name of the workspace to retrieve information about.
+      - When this parameter is used, I(organization_name) must also be provided.
+    type: str
+    required: false
+  organization_name:
+    description:
+      - The name of the organization that contains the workspace.
+      - This parameter is required when I(workspace_name) is provided.
+    type: str
+    required: false
+"""
+EXAMPLES = r"""
+- name: Gather information about a workspace by ID
+  hashicorp.terraform.workspace_info:
+    workspace_id: "ws-sample1234567890"
+  register: workspace_info
+
+- name: Gather information about a workspace by name and organization
+  hashicorp.terraform.workspace_info:
+    workspace_name: "sample_workspace"
+    organization_name: "sample_organization"
+  register: workspace_info
+
+# Example output from workspace_info task:
+# {
+#   "workspace": {
+#     "id": "ws-sample1234567890",
+#     "type": "workspaces",
+#     "attributes": {
+#       "name": "sample_workspace",
+#       "terraform-version": "1.10.5",
+#       "execution-mode": "remote",
+#       "allow-destroy-plan": true,
+#       "auto-apply": false,
+#       "locked": false,
+#       "resource-count": 0,
+#       "created-at": "2025-06-09T09:09:19.872Z",
+#       "updated-at": "2025-07-30T11:15:20.689Z",
+#       "permissions": {
+#         "can-update": true,
+#         "can-destroy": true,
+#         "can-queue-run": true
+#       }
+#     },
+#     "relationships": {
+#       "organization": {
+#         "data": {
+#           "id": "sample_organization",
+#           "type": "organizations"
+#         }
+#       }
+#     }
+#   }
+# } 
+"""
+
+RETURN = r"""
+workspace:
+  type: dict
+  description: A dictionary containing the workspace information.
+  returned: on success
+  contains:
+    id:
+      type: str
+      returned: always
+      description: The unique identifier of the workspace.
+      sample: "ws-sample1234567890"
+    type:
+      type: str
+      returned: always
+      description: The type of the resource (always "workspaces").
+      sample: "workspaces"
+    attributes:
+      type: dict
+      returned: always
+      description: The attributes of the workspace.
+    relationships:
+      type: dict
+      returned: always
+      description: Relationships to other resources.
+    links:
+      type: dict
+      returned: always
+      description: Links related to the workspace.
+      contains:
+        self:
+          type: str
+          returned: always
+          description: API endpoint for this workspace.
+          sample: "/api/v2/organizations/sample_organization/workspaces/sample_workspace"
+        self-html:
+          type: str
+          returned: always
+          description: Web UI URL for this workspace.
+          sample: "api/v2/organizations/sample_organization/workspaces/sample_workspace"
+"""
