2e948acc50ae151119f597ea69f23e610441a724
tanwigeetika1618 <84617407+tanwigeetika1618@users.noreply.github.com>
2025-09-16 13:49:31 +0530

    Adds hashicorp.terraform.view_plan module (#23)
    * Add implementation for plan_info.py module
    
    * Add Task ouputs in example section
    
    * Rename plan_info to plan
    
    * Update auth with common
    
    * Apply single exit point in the module and raise exceptions
    
    * unit-tests
    
    * removed-redundant-tests
    
    * updated-tests
    
    * fixed-lint-errors
    
    * fixted-black-errors
    
    * Update view_plan.py
    
    * delete previous plan_info.py
    
    * Update view_plan.py with proper output rendering
    
    * Refactor plan.py with helper function for common usage
    
    * Better plan.py and view_plan.py
    
    * Fix sonarqube issues
    
    * Adding dataclass decorators
    
    * Fix sonarqube issues
    
    * Add safe checks
    
    * Remove explicit addition of output_format in result dict
    
    * Add unit tests for view_plan.py module
    
    * Modify test_view_plan.py
    
    * Modify unit tests
    
    * Modify unit tests
    
    * Update test_view_plan.py with class EnhancedDummyModule approach
    
    * Fix lint issues
    
    * Better test_plan.py
    
    * Add integration tests for view_plan module
    
    * Fix prettier
    
    * Fix ansible lint errors
    
    * Change project key ib sonar properties
    
    * Fix sanity errors
    
    * Fix sanity
    
    * Update project key in sonar properties
    
    * remove common parameters
    
    Signed-off-by: NilashishC <nchakrab@redhat.com>
    
    * add action plugin
    
    Signed-off-by: NilashishC <nchakrab@redhat.com>
    
    * Update view_plan.py
    
    * Update integration tests to include expected_results.py for comparison
    
    * Add test cases for all helper functions
    
    * Fix lint errors
    
    * Fix lint errors
    
    * Fix lint errors
    
    * Add normalize.json.j2 to stabilize UUIDs in integration tests
    
    * Fix lint errors
    
    * Modify integration tests
    
    * Remove expected_results folder from targets/view_plan
    
    * Add plan.py models for dataclasses
    
    * Fix shenbag error
    
    * Fix sanity
    
    * Fix integration tests
    
    * Update plugins/modules/view_plan.py
    
    Co-authored-by: Shashank Venkat <shvenkat@redhat.com>
    
    * Update plan.py and test_plan.py
    
    * Update test_view_plan.py file
    
    * Added new lines
    
    * Fix lint issues
    
    ---------
    
    Signed-off-by: NilashishC <nchakrab@redhat.com>
    Co-authored-by: Shashank Venkat <shvenkat@redhat.com>
    Co-authored-by: NilashishC <nchakrab@redhat.com>
    (cherry picked from commit 5d1e45662aa0a1081eadd70154f351dc861bae7c)

---
+++ plugins/action/view_plan.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2025 Red Hat
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+from __future__ import absolute_import, division, print_function
+
+
+__metaclass__ = type
+
+from ansible.plugins.action import ActionBase
+
+
+class ActionModule(ActionBase):
+    def run(self, tmp=None, task_vars=None):
+        super(ActionModule, self).run(tmp, task_vars)
+        # this ensures that we render diffed output (when the plan has a diff)
+        # without the need to set diff: True in the task or the --diff flag
+        self._task.diff = True
+        result = self._execute_module(module_args=self._task.args.copy(), task_vars=task_vars, tmp=tmp)
+        return result
