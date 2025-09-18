fcfe7eb745f79e366a5509ccec9e5dfcb66f54d6
Abhishek Chaudhary <abhishekism9450@gmail.com>
2025-09-18 11:30:40 +0530

    adding invalid testcases

--- tests/unit/plugins/modules/test_run_info.py
+++ tests/unit/plugins/modules/test_run_info.py
@@ -31,6 +31,30 @@ class TestRunInfo:
         mock_module.fail_json.assert_called_once_with(msg=str({"status": 404, "data": {"errors": [{"status": "404", "title": "not found"}]}}))
         mock_module.exit_json.assert_not_called()
 
+    @patch("ansible_collections.hashicorp.terraform.plugins.modules.run_info.TerraformClient")
+    @patch("ansible_collections.hashicorp.terraform.plugins.modules.run_info.AnsibleTerraformModule")
+    @patch("ansible_collections.hashicorp.terraform.plugins.module_utils.run.get_run")
+    def test_run_not_found_by_id(self, mock_get_run, mock_ansible_module, mock_terraform_client):
+        from ansible_collections.hashicorp.terraform.plugins.modules.run_info import main
+
+        # Create fake module object
+        run_id = "non-existent"
+        mock_module = Mock()
+        mock_module.check_mode = False
+        mock_module.params = {"run_id": run_id}
+        mock_client = Mock()
+
+        # Configure mocks
+        mock_terraform_client.return_value = mock_client
+        mock_ansible_module.return_value = mock_module
+        mock_get_run.return_value = {}
+
+        # Run the module
+        main()
+        # Assert behavior
+        mock_get_run.assert_called_once_with(client=mock_client, run_id=run_id)
+        mock_module.fail_json.assert_called_once_with(msg=f"The run with ID '{run_id}' was not found.")
+
     @pytest.mark.parametrize(
         "run_info_data,expected_result",
         [
