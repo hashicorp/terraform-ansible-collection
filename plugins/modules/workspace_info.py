41185f2e56cd46ac1c6007dbc2a2fd02a784c971
Kaushiki Singh <kausingh@redhat.com>
2025-10-03 14:36:14 +0530

    Bug fix- add a deepcopy in all modules when creating params from module.params (#63)
    * adding deepcopy
    
    * update test
    
    * adding changelog
    
    * update changelog to address review comment
    
    (cherry picked from commit d802eaef41137cf0f62a6e90f7b1977f086816fc)

--- plugins/modules/workspace_info.py
+++ plugins/modules/workspace_info.py
@@ -278,6 +278,7 @@ workspace:
 """
 
 
+from copy import deepcopy
 from typing import TYPE_CHECKING
 
 from ansible.module_utils._text import to_text
@@ -318,7 +319,7 @@ def main() -> None:
 
     warnings: list[str] = []
     result: Dict[str, Any] = {"changed": False, "warnings": warnings}
-    params: Dict[str, Any] = module.params
+    params: Dict[str, Any] = deepcopy(module.params)
     params["check_mode"] = module.check_mode
     try:
         client = TerraformClient(**module.params)
