7a034ccc8c4ef356e1ce2ce13c8b3935dc2ab877
Shashank Venkat <shvenkat@redhat.com>
2025-10-06 10:24:35 +0530

    updated-version-no

--- plugins/modules/project_info.py
+++ plugins/modules/project_info.py
@@ -8,7 +8,7 @@ __metaclass__ = type
 DOCUMENTATION = r"""
 ---
 module: project_info
-version_added: 1.0.0
+version_added: 1.1.0
 short_description: Gather information about a project in Terraform Enterprise/Cloud.
 author: "Shashank Venkat (@shvenkat)"
 description:
