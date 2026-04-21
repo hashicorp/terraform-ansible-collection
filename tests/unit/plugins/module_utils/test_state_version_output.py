dbcfdde2a7787696d3cdd2a2caada2639ebb8b4f
Prabuddha Chakraborty <prabuddha.chakraborty@hashicorp.com>
2026-04-22 00:11:56 +0530

    Update License to IBM, 2025-2026

--- tests/unit/plugins/module_utils/test_state_version_output.py
+++ tests/unit/plugins/module_utils/test_state_version_output.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2025 Red Hat, Inc.
+# Copyright IBM Corp. 2025, 2026
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from unittest.mock import Mock, patch
